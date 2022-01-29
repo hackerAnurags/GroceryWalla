156d29f19445e2bb3275248f79a1cb2aa45d036c
devpranami <skp.pranami.com>
2022-01-29 12:50:51 +0530

    msg

---
+++ app/src/main/java/com/hackeranushi/grocerywalla/Activities/OtpVerification/GenerateOtp.java
@@ -0,0 +1,16 @@
+package com.hackeranushi.grocerywalla.Activities.OtpVerification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackeranushi.grocerywalla.R;
+
+public class GenerateOtp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_otp);
+    }
+}
---
+++ app/src/main/java/com/hackeranushi/grocerywalla/Activities/OtpVerification/VierifyOtp.java
@@ -0,0 +1,16 @@
+package com.hackeranushi.grocerywalla.Activities.OtpVerification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackeranushi.grocerywalla.R;
+
+public class VierifyOtp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vierify_otp);
+    }
+}
